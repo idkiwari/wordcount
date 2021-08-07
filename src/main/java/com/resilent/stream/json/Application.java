b254509a515fc8edff72b50640487b1fcf27f2ae
Arief Warazuhudien <arief.warazuhudien@bankmandiri.co.id>
2021-08-07 21:39:07 +0700

    initial

--- src/main/java/com/resilent/stream/json/Application.java
+++ src/main/java/com/resilent/stream/json/Application.java
@@ -1,17 +1,27 @@
 package com.resilent.stream.json;
 
+import com.resilient.stream.Flow;
 import com.resilient.stream.Server;
+import com.resilient.stream.http.request.method.Get;
+import com.resilient.stream.http.response.Static;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.File;
 
 public class Application {
 
     static Logger logger = LoggerFactory.getLogger(Application.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
 
         Server server = new Server();
-        
+
+        new Flow(server)
+          .when(new Get("/*").with((s, m, p) -> new File("static","/".equals(p)? "index.html": p).exists()))
+          .then((ctx, r) -> new Static(new File("static","/".equals(ctx.path().plain())? "index.html": ctx.path().plain())));
+
+        server.start();
 
     }
 
